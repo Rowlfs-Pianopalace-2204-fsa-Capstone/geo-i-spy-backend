896f4e0cf941cb0308ef3e1164a6558f3843bbfc
Havin Mickel <12345678900qw@gmail.com>
2022-07-06 10:25:56 -0700

    Added friend routes

--- server/api/users.js
+++ server/api/users.js
@@ -5,7 +5,7 @@ const { requireToken } = require('./gateKeepingMiddleware');
 const { isAdmin } = require('./gateKeepingMiddleware');
 
 const {
-  models: { User },
+  models: { User, Friends },
 } = require('../db');
 module.exports = router;
 
@@ -20,9 +20,48 @@ router.get('/', requireToken, isAdmin, async (req, res, next) => {
   }
 });
 
+router.get('/friends', requireToken, async (req, res, next) => {
+  try {
+    const reponse = await User.findByPk(req.user.id, {
+      include: [
+        {
+          model: User,
+          as: 'friends',
+        },
+      ],
+    });
+    res.send(reponse);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/friend/:id', requireToken, async (req, res, next) => {
+  try {
+    const user = req.user;
+    await user.addFriends(req.params.id);
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/friend/:id', requireToken, async (req, res, next) => {
+  try {
+    const user = req.user;
+    if (user.id === req.params.id) {
+      res.sendStatus(400);
+    } else {
+      await user.removeFriends(req.params.id);
+    }
+    res.sendStatus(200);
+  } catch (error) {
+    next(error);
+  }
+});
 router.get('/:id', requireToken, isAdmin, async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.id);
+    const user = await User.findByPk(req.user.id);
     res.json(user);
   } catch (error) {
     next(error);
@@ -41,7 +80,7 @@ router.delete('/:id', requireToken, isAdmin, async (req, res, next) => {
 
 router.put('/:id', requireToken, isAdmin, async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.id);
+    const user = req.user;
     const updatedInformation = {
       username: req.body.username || user.username,
       img_url: req.body.img_url || user.img_url,
