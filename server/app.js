0233a0a5bd3f2090733f7cb3aef2e8ff9a7337c3
ColinKetcham <42621093+ColinKetcham@users.noreply.github.com>
2022-07-08 10:55:37 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -17,23 +17,22 @@ app.use(express.json());
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
 
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 //app.use(bodyParser.urlencoded({ extended: false }));
 
-//app.use(
-//  bodyParser.json({
-//    limit: '10mb',
-//  })
-//);
-
-//app.use(
-//  bodyParser.urlencoded({
-//    limit: '10mb',
-//    parameterLimit: 10000000,
-//    extended: true,
-//  })
-//);
+app.use(
+  bodyParser.json({
+    limit: '10mb',
+  })
+);
+app.use(
+  bodyParser.urlencoded({
+    limit: '10mb',
+    parameterLimit: '10mb',
+    extended: true,
+  })
+);
 
 
 
