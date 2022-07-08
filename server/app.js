ca8855bc4882cd356adaebe628e7cb3161abf129
ColinKetcham <42621093+ColinKetcham@users.noreply.github.com>
2022-07-08 11:22:05 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ module.exports = app;
 
 app.use(morgan('dev'));
 
-//app.use(express.json());
+app.use(express.json());
 
-app.use(express.json({limit: '2mb', extended: true}));
+app.use(express.json({limit: '20mb', extended: true}));
 app.use(express.urlencoded({limit: '23mb', extended: true}));
 
 
