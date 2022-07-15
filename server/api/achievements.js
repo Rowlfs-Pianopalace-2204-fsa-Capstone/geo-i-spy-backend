9713f00a0e31cb30813d094a2fb42f17c2dc56a7
Havin Mickel <12345678900qw@gmail.com>
2022-07-15 06:15:15 -0700

    sockets ready for feed

--- server/api/achievements.js
+++ server/api/achievements.js
@@ -11,64 +11,14 @@ const {
 const User = require('../db/models/user');
 const Achievements = require('../db/models/achievements');
 const e = require('express');
+const getFeed = require('../socket-functions');
 const cloudinary = require('cloudinary').v2;
 module.exports = router;
 
 router.get('/feed', requireToken, async (req, res, next) => {
   try {
-    const reponse = await User.findByPk(req.user.id, {
-      attributes: ['id', 'username', 'img_url'],
-      include: [
-        { model: Challenge },
-        {
-          model: User,
-          as: 'followers',
-          attributes: ['id', 'username', 'img_url'],
-          include: {
-            model: Challenge,
-          },
-        },
-      ],
-    });
-    const allFollowingAchievements = [];
-    let i = 0;
-    while (reponse.followers.length > i) {
-      if (reponse.followers[0].challenges[0]) {
-        const temp = {
-          challenge: reponse.followers[0].challenges[0],
-          username: reponse.followers[0].username,
-          id: reponse.followers[0].id,
-          img: reponse.followers[0].img_url,
-        };
-        allFollowingAchievements.push(temp);
-        reponse.followers[0].challenges.shift();
-      } else {
-        reponse.followers.shift();
-      }
-    }
-    i = 0;
-    while (reponse.challenges.length > i) {
-      if (reponse.challenges[0]) {
-        const temp = {
-          challenge: reponse.challenges[0],
-          username: reponse.username,
-          id: reponse.id,
-          img: reponse.img_url,
-        };
-        allFollowingAchievements.push(temp);
-        reponse.challenges.shift();
-      } else {
-        reponse.challenges.shift();
-      }
-    }
-    // allFollowingAchievements.push(reponse);
-    allFollowingAchievements.sort(function (x, y) {
-      return (
-        new Date(x.challenge.Achievement.createAt) -
-        new Date(y.challenge.Achievement.creatAT)
-      );
-    });
-    res.send(allFollowingAchievements);
+    const reponse = await getFeed(req.user.id);
+    res.send(reponse);
   } catch (err) {
     next(err);
   }
---
+++ server/socket-functions.js
@@ -0,0 +1,64 @@
+/** @format */
+
+const Challenge = require('./db/models/challenges');
+const User = require('./db/models/user');
+
+const getFeed = async (id) => {
+  const reponse = await User.findByPk(id, {
+    attributes: ['id', 'username', 'img_url'],
+    include: [
+      { model: Challenge },
+      {
+        model: User,
+        as: 'followers',
+        attributes: ['id', 'username', 'img_url'],
+        include: {
+          model: Challenge,
+        },
+      },
+    ],
+  });
+
+  const allFollowingAchievements = [];
+  let i = 0;
+  while (reponse.followers.length > i) {
+    if (reponse.followers[0].challenges[0]) {
+      const temp = {
+        challenge: reponse.followers[0].challenges[0],
+        username: reponse.followers[0].username,
+        id: reponse.followers[0].id,
+        img: reponse.followers[0].img_url,
+      };
+      allFollowingAchievements.push(temp);
+      reponse.followers[0].challenges.shift();
+    } else {
+      reponse.followers.shift();
+    }
+  }
+  i = 0;
+  while (reponse.challenges.length > i) {
+    if (reponse.challenges[0]) {
+      const temp = {
+        challenge: reponse.challenges[0],
+        username: reponse.username,
+        id: reponse.id,
+        img: reponse.img_url,
+      };
+      allFollowingAchievements.push(temp);
+      reponse.challenges.shift();
+    } else {
+      reponse.challenges.shift();
+    }
+  }
+  // allFollowingAchievements.push(reponse);
+  allFollowingAchievements.sort(function (x, y) {
+    return (
+      new Date(x.challenge.Achievement.createAt) -
+      new Date(y.challenge.Achievement.creatAT)
+    );
+  });
+  console.log(allFollowingAchievements);
+  return allFollowingAchievements;
+};
+
+module.exports = getFeed;
