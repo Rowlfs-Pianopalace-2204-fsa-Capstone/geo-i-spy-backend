3cd67328385cfad7d53f398b932b43734eb2eed6
Havin Mickel <12345678900qw@gmail.com>
2022-07-15 12:57:44 -0700

    added rooms to index

--- server/api/index.js
+++ server/api/index.js
@@ -8,6 +8,7 @@ router.use('/challenges', require('./challenges'));
 router.use('/followers', require('./followers'));
 router.use('/achievements', require('./achievements'));
 router.use('/messages', require('./messages'));
+router.use('/rooms', require('./rooms'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
